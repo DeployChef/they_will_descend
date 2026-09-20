d4bb154906d5f998e4677327a59e5cc60a29bb6a
SHIKAKA\Valkn <Valknut29@gmail.com>
2026-09-20 23:51:42 +0300

    Камера отлетает + шейдер один скачал.

--- TheyWillDescend/Assets/_Project/Scripts/Presentation/ShellUi/PauseMenuScreen.cs
+++ TheyWillDescend/Assets/_Project/Scripts/Presentation/ShellUi/PauseMenuScreen.cs
@@ -4,7 +4,6 @@ using TheyWillDescend.Presentation.Agents;
 using TheyWillDescend.Presentation.Cameras;
 using TheyWillDescend.Presentation.City;
 using TheyWillDescend.Presentation.GameHud;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -40,8 +39,6 @@ namespace TheyWillDescend.Presentation.ShellUi
         void Awake()
         {
             Current = this;
-            if (continueButton == null)
-                BuildChrome();
             Bind(continueButton, () => ContinueClicked?.Invoke());
             Bind(saveButton, () => SaveClicked?.Invoke());
             Bind(loadButton, () => LoadClicked?.Invoke());
@@ -88,71 +85,6 @@ namespace TheyWillDescend.Presentation.ShellUi
         {
             if (button != null)
                 button.onClick.AddListener(action);
-        }
-
-        void BuildChrome()
-        {
-            var dim = GetComponent<Image>();
-            if (dim == null)
-                dim = gameObject.AddComponent<Image>();
-            dim.color = new Color(0f, 0f, 0f, 0.65f);
-            dim.raycastTarget = true;
-
-            var card = new GameObject("MenuCard", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
-            card.transform.SetParent(transform, false);
-            var cardRt = (RectTransform)card.transform;
-            cardRt.anchorMin = cardRt.anchorMax = new Vector2(0.5f, 0.5f);
-            cardRt.pivot = new Vector2(0.5f, 0.5f);
-            cardRt.sizeDelta = new Vector2(320f, 292f);
-            card.GetComponent<Image>().color = new Color(0.12f, 0.12f, 0.14f, 0.96f);
-
-            var layout = card.AddComponent<VerticalLayoutGroup>();
-            layout.padding = new RectOffset(20, 20, 20, 20);
-            layout.spacing = 12f;
-            layout.childAlignment = TextAnchor.MiddleCenter;
-            layout.childControlWidth = true;
-            layout.childControlHeight = true;
-            layout.childForceExpandWidth = true;
-            layout.childForceExpandHeight = false;
-
-            continueButton = CreateButton(card.transform, "Continue");
-            saveButton = CreateButton(card.transform, "Save");
-            loadButton = CreateButton(card.transform, "Load");
-            mainMenuButton = CreateButton(card.transform, "Main Menu");
-        }
-
-        static Button CreateButton(Transform parent, string label)
-        {
-            var go = new GameObject(label + "Button", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
-            go.transform.SetParent(parent, false);
-            var image = go.GetComponent<Image>();
-            image.color = new Color(0.18f, 0.18f, 0.2f, 0.92f);
-            var layout = go.AddComponent<LayoutElement>();
-            layout.minHeight = 48f;
-            layout.preferredHeight = 48f;
-            var button = go.AddComponent<Button>();
-            button.targetGraphic = image;
-            var colors = button.colors;
-            colors.highlightedColor = new Color(0.85f, 0.9f, 1f, 1f);
-            colors.pressedColor = new Color(0.7f, 0.75f, 0.85f, 1f);
-            button.colors = colors;
-
-            var textGo = new GameObject("Label", typeof(RectTransform), typeof(CanvasRenderer));
-            textGo.transform.SetParent(go.transform, false);
-            var textRt = (RectTransform)textGo.transform;
-            textRt.anchorMin = Vector2.zero;
-            textRt.anchorMax = Vector2.one;
-            textRt.offsetMin = Vector2.zero;
-            textRt.offsetMax = Vector2.zero;
-            var tmp = textGo.AddComponent<TextMeshProUGUI>();
-            tmp.text = label;
-            tmp.fontSize = 22f;
-            tmp.alignment = TextAlignmentOptions.Center;
-            tmp.color = Color.white;
-            tmp.raycastTarget = false;
-            if (TMP_Settings.defaultFontAsset != null)
-                tmp.font = TMP_Settings.defaultFontAsset;
-            return button;
         }
     }
 }
